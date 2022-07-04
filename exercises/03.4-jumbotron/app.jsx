27ad19f914ac58c9d2988930b0786606d07453d6
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-07-04 20:44:48 +0000

    monday

--- exercises/03.4-jumbotron/app.jsx
+++ exercises/03.4-jumbotron/app.jsx
@@ -4,13 +4,15 @@ import PropTypes from "prop-types";
 
 const Jumbotron = (props) => {
 	//here you have to return expected html using the properties being passed to the component
-	<div className="jumbotron m-5">
-		<h1 className="display-4">{props.title}</h1>
-		<p className="lead">{props.description}</p>
-		<a className="btn btn-primary btn-lg" href={props.buttonURL} role="button">
-			{props.buttonLabel}
-		</a>
-	</div>;
+	return (
+		<div className="jumbotron m-5">
+			<h1 className="display-4">{props.title}</h1>
+			<p className="lead">{props.description}</p>
+			<a className="btn btn-primary btn-lg" href={props.buttonURL} role="button">
+				{props.buttonLabel}
+			</a>
+		</div>
+	);
 };
 
 Jumbotron.propTypes = {
@@ -18,7 +20,7 @@ Jumbotron.propTypes = {
 	title: PropTypes.string,
 	description: PropTypes.string,
 	buttonLabel: PropTypes.string,
-	buttonURL: PropTypes.string
+	buttonURL: PropTypes.string,
 };
 
 ReactDOM.render(
@@ -26,7 +28,7 @@ ReactDOM.render(
 		title="Welcome to react"
 		description="React is the most popular rendering library in the world"
 		buttonLabel="Go to the official website"
-		buttonURL={"https://reactjs.org/"}
+		buttonURL="https://reactjs.org/"
 	/>,
 
 	document.querySelector("#myDiv")
